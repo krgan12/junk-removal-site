cc9b9c05e68acf221dc8b893cdb987c701740587
Keagan Rodrigues <keagan123@outlook.com>
2026-07-04 21:21:37 -0400

    Checkpoint

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {/* <Header /> */}
-        {children}
+        {children} 
         {/* Footer is different for ALL pages */}
       </body>
     </html>
