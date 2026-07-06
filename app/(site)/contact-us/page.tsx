f851b8dcf4a874f63cd1604b4df9fb23e468c106
Keagan Rodrigues <keagan123@outlook.com>
2026-07-05 23:51:21 -0400

    Form section basic code done

--- app/(site)/contact-us/page.tsx
+++ app/(site)/contact-us/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import ContactUsFooter from '@/components/ContactUsFooter'
-import React from 'react'
+import React, { useState } from 'react'
 
 function page() {
 
   const HEADER_OFFSET = 20;
+
+  const [email, setEmail] = useState("");
+  const [emailTouched, setEmailTouched] = useState(false);
 
   return (
     <div className='min-h-screen'>
@@ -24,16 +28,55 @@ function page() {
         </section>
 
         <div className='mx-auto flex max-w-[610px] flex-col items-center'>
-          <h1>JunkBGone Junk Removal - Get a free Quote</h1>
-          <p>Get a free junk removal quite! Tell us your city, nearest intersection, and what you need removed
-            (example: 1 couch, 10 bags). You can also text the details to: <span className='underline'>905-781-4046</span>
+          
+          <h1 className='text-center text-[58px] leading-[1.15] font-normal tracking-[-1px] text-[#222]'>JunkBGone Junk Removal - Get a free Quote</h1>
+          
+          <p className='mt-8 text-center text-[19px] leading-10 text-[#222]'>Get a free junk removal quite! Tell us your city, nearest intersection, and what you need removed
+          (example: 1 couch, 10 bags). You can also text the details to: <span className='underline cursor-pointer text-[#4d69b3]'>905-781-4046.</span>
           </p>
-          <form>
-            <label htmlFor=""></label>
-            {/* I am human hCaptch verification - to do NEAR END  */}
-            {/* Submit Button */}
+          <form className='mt-10 flex w-full flex-col'>
+
+            <div className='mb-7'>
+              
+              <label className='mb-2 block text-[16px] font-normal text-[black]' htmlFor="">Name</label>
+              <input placeholder='Enter your name' className='h-[50px] w-full border rounded-sm border-[#dddddd] px-4 text-[16px] placeholder:text-[#777] outline-none transition-colors focus:border-[#bdbdbd]' />
+
+              <label className="mb-2 mt-4 block text-[16px]">E-mail <span className='text-red-600'>*</span></label>
+              <input
+               value={email}
+               onChange={(e) => setEmail(e.target.value)}
+               onBlur={() => setEmailTouched(true)}
+               placeholder='Enter your email' 
+               className='h-[50px] w-full border rounded-sm border-[#dddddd] px-4 text-[16px] placeholder:text-[#777] outline-none transition-colors focus:border-[#bdbdbd]' />
+
+              {emailTouched && email.trim() === "" && (
+                <p className='mt-2 text-[16px] text-red-600'>This field cannot be empty.</p>
+              )}
+
+              <label className="mb-2 mt-4 block text-[16px]">Phone</label>
+              <input placeholder='Enter your phone number' className='h-[50px] w-full border rounded-sm border-[#dddddd] px-4 text-[16px] placeholder:text-[#777] outline-none transition-colors focus:border-[#bdbdbd]' />
+
+              <label className="mb-2 mt-4 block text-[16px]">Message</label>
+              <textarea rows={4} placeholder='Enter your message' className='min-h-[90px] w-full
+               border rounded-sm border-[#dddddd] px-4 py-3 text-[16px] placeholder:text-[#777]
+              outline-none transition-colors focus:border-[#bdbdbd]'>
+
+              </textarea>
+
+              <div className='mt-4 h-[78px] w-[304px] border border-[$d9d9d9] bg-white'></div>
+              
+              <button type='submit' className='hover:cursor-pointer mt-6 h-[48px] w-[120px] bg-[#e32726] text-[18px] font-normal text-white transition-colors hover:bg-[#cf201f]'>Submit</button>
+            </div>
+           
           </form>
         </div>
+        <ContactUsFooter />
+        <ContactUsFooter />
+        <ContactUsFooter />
+        <ContactUsFooter />
+        <ContactUsFooter />
+        <ContactUsFooter />
+        <ContactUsFooter />
         <ContactUsFooter />
 
     </div>
