963d0ecfeb8a8d5581992ce8b71a28583bb31d84
Keagan Rodrigues <keagan123@outlook.com>
2026-07-05 23:19:01 -0400

    cooler skeleton website

--- app/(site)/contact-us/page.tsx
+++ app/(site)/contact-us/page.tsx
@@ -2,10 +2,40 @@ import ContactUsFooter from '@/components/ContactUsFooter'
 import React from 'react'
 
 function page() {
+
+  const HEADER_OFFSET = 20;
+
   return (
-    <div>
-        Contact Us Page
+    <div className='min-h-screen'>
+      {/* <div>
+          <h1>JunkBGone Junk Removal - Get a free Quote</h1>
+          <p>Get a free junk removal quite! Tell us your city, nearest intersection, and what you need removed
+            (example: 1 couch, 10 bags). You can also text the details to: <span className='underline'>905-781-4046</span>
+          </p>
+          <form>
+            <label htmlFor=""></label>
+            I am human hCaptch verification - to do NEAR END 
+            Submit Button
+          </form>
+        </div>
+        <ContactUsFooter /> */}
+
+        <section className='mx-auto max-w-[610px]' style={{paddingTop: `${HEADER_OFFSET}vh`}}>
+        </section>
+
+        <div className='mx-auto flex max-w-[610px] flex-col items-center'>
+          <h1>JunkBGone Junk Removal - Get a free Quote</h1>
+          <p>Get a free junk removal quite! Tell us your city, nearest intersection, and what you need removed
+            (example: 1 couch, 10 bags). You can also text the details to: <span className='underline'>905-781-4046</span>
+          </p>
+          <form>
+            <label htmlFor=""></label>
+            {/* I am human hCaptch verification - to do NEAR END  */}
+            {/* Submit Button */}
+          </form>
+        </div>
         <ContactUsFooter />
+
     </div>
   )
 }
