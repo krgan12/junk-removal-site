dccbb03de8366b803bed8dfbd96865d1c474fcd0
Keagan Rodrigues <keagan123@outlook.com>
2026-07-06 22:46:53 -0400

    UI changes. Logo at the bottom done

--- components/ContactUsFooter.tsx
+++ components/ContactUsFooter.tsx
@@ -24,14 +24,14 @@ async function ContactUsFooter() {
           className='object-cover' 
         />
        <h2 className='mt-5 ml-2 text-[40px] text-white leading-[1.15] font-medium tracking-[-1px]
-              text-[#222]'>
-                Ready to Clear Your Space?
-            </h2>
+          text-[#222]'>
+          Ready to Clear Your Space?
+        </h2>
          
-          <p className='leading-8 text-[18px] text-white font-normal max-w-[550px]'>Contact Junk2Go today for fast, affordable junk removal in Hamilton and beyond! Call/text us at 289-456-8076 or email junk2gohamilton@gmail.com</p>
+        <p className='leading-8 text-[18px] text-white font-normal max-w-[550px]'>Contact Junk2Go today for fast, affordable junk removal in Hamilton and beyond! Call/text us at 289-456-8076 or email junk2gohamilton@gmail.com</p>
       </div>
-      <div className='bg-black'>
-        <p className='text-gray-600'>© 2026 JunkBGone - Junk Removal Service</p>
+      <div className='bg-[#131416] items-center h-[212px] relative'>
+        <p className='text-[#727374] text-[18px] text-center relative top-40 '>© 2026 JunkBGone - Junk Removal Service</p>
       </div>
     </div>
   )
