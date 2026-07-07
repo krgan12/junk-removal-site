693c1391392ea6b0e98ff1f34d4850f10faaaeb1
Keagan Rodrigues <keagan123@outlook.com>
2026-07-07 15:12:34 -0400

    Header format changed

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,9 +12,10 @@ function Header() {
     <div>
       <a href="/"><h1 className='hover:cursor-pointer'>Junk B Gone - Junk Removal Service</h1> </a>&nbsp;
       <Link href="/" className={pathname === '/' ? 'text-red-500': 'text-black'}>Home</Link> &nbsp;
-      <Link href="/services" className={pathname === '/services' ? 'text-red-500' : 'text-black'}>Services</Link> &nbsp;
+      <Link href="/contact-us" className={pathname === '/contact-us' ? 'text-red-500': 'text-black'}>Contact Us</Link>&nbsp;
       <Link href="/service-area" className={pathname === '/service-area' ? 'text-red-500' : 'text-black'}>Service Area</Link>&nbsp;
-      <Link href="/contact-us" className={pathname === '/contact-us' ? 'text-red-500': 'text-black'}>Contact Us</Link>
+      <Link href="/services" className={pathname === '/services' ? 'text-red-500' : 'text-black'}>Services</Link> &nbsp; 
+      
     </div>
   )
 }
