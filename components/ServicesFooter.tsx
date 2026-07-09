d8710c71d2a6d4e7140c51152aa451fabb82fa6b
Keagan Rodrigues <keagan123@outlook.com>
2026-07-09 19:22:53 -0400

    Awesomegit add .git add . Services section is officially done

--- components/ServicesFooter.tsx
+++ components/ServicesFooter.tsx
@@ -5,33 +5,39 @@ function ServicesFooter() {
   return (
     <footer className='mt-32 w-full bg-white'>
 
-      <div className='mx-auto max-w-[1250px] text-center'>
+      <div className='mx-auto max-w-[1250px]'>
 
-        <h2 className='text-[30px] tracking-[-1.5px] font-normal text-[#222]'>
+        <div className='flex items-center justify-between'>
+
+        <a href="/"><h2 className='text-[30px] tracking-[-1.5px] font-normal text-[#222]'>
           JunkBGone - Junk Removal Service
-        </h2>
+        </h2></a>
 
-        <div className='mt-8 flex justify-center gap-10'>
+        {/* <div className='mt-8 flex justify-center gap-10'> */}
+        <div className='flex gap-10'>
           
-          <a href="/" className='text-[18px] text-[#102356] transition-colors duration-300 hover:text-[#db2424]'>Home</a>
+          <a href="/" className='text-[18px] text-[#536aa1] transition-colors duration-300 hover:text-[#db2424]'>Home</a>
 
-          <a href="/contact-us" className='text-[18px] text-[#102356] transition-colors duration-300 hover:text-[#db2424]'>Contact</a>
+          <a href="/contact-us" className='text-[18px] text-[#536aa1] transition-colors duration-300 hover:text-[#db2424]'>Contact</a>
 
         </div>
 
-         <div className='mt-12 border-t border-[#dddddd]'></div>
+      </div>
+
+        <div className='mt-6 border-t border-[#dddddd]'></div>
 
 
-        <div className='mt-10 flex justify-center'>
+        <div className='mt-6 mb-10 flex items-center justify-between'>
+
+          <p className='py-8 pt-0 text-[17.5px] text-[#666]'>© 2026 JunkBGone - Junk Removal Service</p>
 
           <SocialIcons_Home />
 
         </div>
 
-       
-        <p className='py-8 text-[16px] text-[#666]'>© 2026 JunkBGone - Junk Removal Service</p>
 
       </div>
+      
 
     </footer>
   )
