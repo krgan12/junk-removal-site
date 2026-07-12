bc782809f1a2ebe2d4151cdcaee4809b6d5b23c5
Keagan Rodrigues <keagan123@outlook.com>
2026-07-12 00:21:55 -0400

    Small icons added

--- app/PageClient.tsx
+++ app/PageClient.tsx
@@ -6,6 +6,7 @@ import HomeImgColl3 from "@/components/HomeImgColl3";
 import { client } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
 import { homeQuery } from "@/sanity/queries/home";
+import { Clock, Mail, MailIcon, MapIcon, MapPin, Phone } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 // import { useState } from "react";
@@ -210,43 +211,12 @@ export default async function Page({
       </div>
 
       <div>
+        {/* <MapIcon /> --> for NEXT section */}
+        <MapPin className="text-white fill-gray-500"/>
+        <Phone className="fill-gray-500 text-white"/>
+        <MailIcon className="text-white fill-gray-500"/>
+        <Clock className="fill-gray-500 text-white" />
         {/* Div to work on */}
-
-         {/* <form className='mt-10 flex w-full flex-col max-w-[610px]'>
-
-            <div className='mb-7'>
-              
-              <label className='mb-2 block text-[16px] font-normal text-[black]' htmlFor="">Name</label>
-              <input placeholder='Enter your name' className='h-[50px] w-full border rounded-sm border-[#dddddd] px-4 text-[16px] placeholder:text-[#777] outline-none transition-colors focus:border-[#bdbdbd]' />
-
-              <label className="mb-2 mt-4 block text-[16px]">E-mail <span className='text-red-600'>*</span></label>
-              <input
-               value={email}
-               onChange={(e) => setEmail(e.target.value)}
-               onBlur={() => setEmailTouched(true)}
-               placeholder='Enter your email' 
-               className='h-[50px] w-full border rounded-sm border-[#dddddd] px-4 text-[16px] placeholder:text-[#777] outline-none transition-colors focus:border-[#bdbdbd]' />
-
-              {emailTouched && email.trim() === "" && (
-                <p className='mt-2 text-[16px] text-red-600'>This field cannot be empty.</p>
-              )}
-
-              <label className="mb-2 mt-4 block text-[16px]">Phone</label>
-              <input placeholder='Enter your phone number' className='h-[50px] w-full border rounded-sm border-[#dddddd] px-4 text-[16px] placeholder:text-[#777] outline-none transition-colors focus:border-[#bdbdbd]' />
-
-              <label className="mb-2 mt-4 block text-[16px]">Message</label>
-              <textarea rows={2} placeholder='Enter your message' className='min-h-[45px] w-full
-               border rounded-sm border-[#dddddd] px-4 py-3 text-[16px] placeholder:text-[#777]
-              outline-none transition-colors focus:border-[#bdbdbd]'>
-
-              </textarea>
-
-              <div className='mt-4 h-[55px] w-[304px] border border-[#d9d9d9] bg-white'></div>
-              
-              <button type='submit' className='hover:cursor-pointer mt-6 h-[48px] w-[120px] bg-[#e32726] text-[18px] font-normal text-white transition-colors hover:bg-[#cf201f]'>Submit</button>
-            </div>
-           
-          </form> */}
           {form}
       </div>
 
