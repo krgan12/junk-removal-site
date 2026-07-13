f4f049e790efca2f29e5f022d2c83a09c61bdf57
Keagan Rodrigues <keagan123@outlook.com>
2026-07-13 18:17:59 -0400

    safety commit - FALLBACK if all goes south

--- app/PageServer.tsx
+++ app/PageServer.tsx
@@ -39,6 +39,10 @@ export default function PageServer() {
               </textarea>
 
               <div className='mt-4 h-[55px] w-[304px] border border-[#d9d9d9] bg-white'></div>
+
+              {/* <div className="h-captcha" data-sitekey="e38e91b6-41b8-443c-85b0-3490aa4ab272"></div>
+              <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+             */}
               
               <button type='submit' className='hover:cursor-pointer mt-6 h-[48px] w-[120px] bg-[#e32726] text-[18px] font-normal text-white transition-colors hover:bg-[#cf201f]'>Submit</button>
             </div>
