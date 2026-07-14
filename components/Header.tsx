8f6b05744e5bd73490fb1d29679d6e7c527ec57a
Keagan Rodrigues <keagan123@outlook.com>
2026-07-14 18:01:54 -0400

    UI for header finished. Functionality pending

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,16 +9,17 @@ function Header() {
 
   return (
     // fixed is RESPONSIBLE for the transparent header
-    <div className='fixed z-50 border border-red-500 bg-white w-full h-[12.56%] pt-9 space-x-20 pl-20 items-center text-center'>
+    <div className='fixed z-50 border border-red-500 bg-white w-full h-[12.56%] pt-9 space-x-10 pl-20 items-center text-center'>
       
-      <Link href="/" className='hover:cursor-pointer space-x-70 text-[24px] right-10 relative'>
+      <Link href="/" className='hover:cursor-pointer space-x-70 text-[24px] right-55.5 relative'>
         JunkBGone - Junk Removal Service
       </Link>&nbsp;
-
-      <Link href="/" className={pathname === '/' ? 'text-red-500': 'text-black'}>Home</Link> &nbsp;
-      <Link href="/contact-us" className={pathname === '/contact-us' ? 'text-red-500': 'text-black'}>Contact Us</Link>&nbsp;
-      <Link href="/service-area" className={pathname === '/service-area' ? 'text-red-500' : 'text-black'}>Service Area</Link>&nbsp;
-      <Link href="/services" className={pathname === '/services' ? 'text-red-500' : 'text-black'}>Services</Link> &nbsp;
+      <div className='inline-block space-x-10 relative left-31'>
+      <Link href="/" className={pathname === '/' ? 'text-red-500 text-[106%] relative': 'text-black text-[106%]'}>Home</Link> &nbsp;
+      <Link href="/contact-us" className={pathname === '/contact-us' ? 'text-red-500 text-[106%]': 'text-black text-[106%]'}>Contact Us</Link>&nbsp;
+      <Link href="/service-area" className={pathname === '/service-area' ? 'text-red-500 text-[106%]' : 'text-black text-[106%]'}>Service Area</Link>&nbsp;
+      <Link href="/services" className={pathname === '/services' ? 'text-red-500 text-[106%]' : 'text-black text-[106%]'}>Services</Link> &nbsp;
+      </div>
     </div>
   )
 }
