62d3bbf1dc9559f622456da0223b8e7432e787a5
Keagan Rodrigues <keagan123@outlook.com>
2026-07-14 17:46:17 -0400

    Header debugging

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,8 +9,12 @@ function Header() {
 
   return (
     // fixed is RESPONSIBLE for the transparent header
-    <div className='fixed z-50 bg-white w-full h-[12.56%] pt-10 space-x-20 pl-20 items-center text-center'>
-      <Link href="/" className='hover:cursor-pointer space-x-70'>Junk B Gone - Junk Removal Service</Link>&nbsp;
+    <div className='fixed z-50 border border-red-500 bg-white w-full h-[12.56%] pt-9 space-x-20 pl-20 items-center text-center'>
+      
+      <Link href="/" className='hover:cursor-pointer space-x-70 text-[24px] right-10 relative'>
+        JunkBGone - Junk Removal Service
+      </Link>&nbsp;
+
       <Link href="/" className={pathname === '/' ? 'text-red-500': 'text-black'}>Home</Link> &nbsp;
       <Link href="/contact-us" className={pathname === '/contact-us' ? 'text-red-500': 'text-black'}>Contact Us</Link>&nbsp;
       <Link href="/service-area" className={pathname === '/service-area' ? 'text-red-500' : 'text-black'}>Service Area</Link>&nbsp;
