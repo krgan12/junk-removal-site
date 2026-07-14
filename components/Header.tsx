16da9b3ba4bb27f15fcb34f317a435f43d966e6b
Keagan Rodrigues <keagan123@outlook.com>
2026-07-14 17:39:52 -0400

    Safety commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ function Header() {
   return (
     // fixed is RESPONSIBLE for the transparent header
     <div className='fixed z-50 bg-white w-full h-[12.56%]'>
-      <a href="/"><h1 className='hover:cursor-pointer'>Junk B Gone - Junk Removal Service</h1> </a>&nbsp;
+      <Link href="/" className='hover:cursor-pointer'>Junk B Gone - Junk Removal Service</Link>&nbsp;
       <Link href="/" className={pathname === '/' ? 'text-red-500': 'text-black'}>Home</Link> &nbsp;
       <Link href="/contact-us" className={pathname === '/contact-us' ? 'text-red-500': 'text-black'}>Contact Us</Link>&nbsp;
       <Link href="/service-area" className={pathname === '/service-area' ? 'text-red-500' : 'text-black'}>Service Area</Link>&nbsp;
