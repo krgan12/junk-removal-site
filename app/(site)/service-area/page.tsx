dff0f683e2c4d9676eb690b03ef04474ae5f2a6a
Keagan Rodrigues <keagan123@outlook.com>
2026-07-21 02:31:10 -0400

    Section cards added

--- app/(site)/service-area/page.tsx
+++ app/(site)/service-area/page.tsx
@@ -40,11 +40,11 @@ async function page() {
           <p className='mt-4 text-[17px] leading-7 text-[#666] max-w-[546.5px] font-medium'>
             Not sure if we service your area?
             Give us a call at 
-            &nbsp;
+            {" "}
             <span className='font-semibold text-[#50659b] underline'>
               <a href="tel:+1-905-905-781-4046" target='_blank'>905-781-4046</a>
             </span>
-            &nbsp;
+            {" "}
             or text us a photo of what you need removed for a fast, free quote.
             We're open 7 days a week and ready to help.
           </p>
@@ -82,6 +82,44 @@ async function page() {
 
             <h2 className='mt-5 max-w-[2000px] text-center text-[48px] leading-[1.12] 
             tracking-[-1.3px] font-normal text-[#222]'>Service Areas - Oakville & Surrounding Communities</h2>
+
+            <p className='mt-7 max-w-[950px] text-center text-[18px] leading-8 text-[#666]'>
+              Not sure if we service your area? Give us a call at
+              {" "}<span className='font-semibold text-[#50659b] underline'>905-781-4046</span>{" "}
+              or text us a photo of what you need removed for a fast, free quote. 
+              We're open 7 days a week and ready to help.
+            </p>
+
+            <div className='mt-20 grid grid-cols-3 gap-8'>
+
+              <div className='flex flex-col items-center bg-[#f7f7f7] px-10 py-12 min-h-[360px]'>
+
+                <h3 className='text-[30px] leading-tight text-[#50659b] underline font-semibold'>Junk Removal Oakville</h3>
+
+                <p className='mt-6 text-center text-[17px] leading-8 text-[#666] text-center'>
+                  We proudly serve all Oakville neighbourhoods including Downtown Oakville, South Oakville, Mid Oakville, and North Oakville. 
+                  Whether it's a garage cleanout, appliance removal, or full estate cleanout, we got you covered.
+                </p>
+
+                
+              </div>       
+
+
+            </div>
+
+            <div className='mt-100 gap-10 flex'>
+                  <Link href="/contact-us">
+                      <button className='h-[46px] w-[165px] bg-[#da2424] text-white text-[16px] transition-colors hover:bg-[#cf201f]
+                      hover:cursor-pointer'
+                      >Get a Quote
+                    </button>
+                  </Link>
+                  <Link href="/contact-us">
+                    <button className='h-[46px] w-[165px] bg-[#da2424] text-white text-[16px] transition-colors hover:bg-[#cf201f]
+                      hover:cursor-pointer'
+                    >Free Estimates</button>
+                  </Link>
+                </div>
 
           </div>
 
